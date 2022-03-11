6d871bda1ae6c8c86ffe0f32674fe364c836336f
lizmwale-3 <elizabethmwale3@gmail.com>
2022-03-11 04:13:09 +0300

    My variables

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main - entry point
- *My printf task
- *Return: 1 after program execution
+ *main - Entry point
+ *My hello, printf task
+ *Return: return 1 after program execution
  */
 int main(void)
 {
