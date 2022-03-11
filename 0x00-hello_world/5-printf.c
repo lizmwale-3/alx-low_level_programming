847e15daed2abffa73a3e4910176568575be7d09
lizmwale-3 <elizabethmwale3@gmail.com>
2022-03-11 04:35:42 +0300

    My variables

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
  *main - Entry point
  *My hello, printf task
@@ -8,4 +8,4 @@ int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-} 
+}
