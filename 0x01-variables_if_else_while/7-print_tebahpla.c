aa729de3357884ca96b22bbdef7d712b373c76ea
lizmwale-3 <elizabethmwale3@gmail.com>
2022-03-18 01:39:47 +0300

    My print code

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio,h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correction)
+ */
+
+int main(void)
+
+{
+char alphabet;
+
+for (alphabt = 122 ; alphabet >= 97; alphabet--)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+} 
