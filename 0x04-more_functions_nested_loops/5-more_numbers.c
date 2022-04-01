6b366908550ce922fe2c62b31e04a0a40ce57af7
lizmwale-3 <elizabethmwale3@gmail.com>
2022-04-02 01:41:06 +0300

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,7 +12,9 @@ void more_numbers(void)
 {
 int number, tens, units, row;
 
-for (row = 1; row <= 14; number++)
+for (row = 1; row <= 10; row++)
+{
+for (number = 0; number <= 14; number++)
 {
 tens = number / 10;
 units = number % 10;
