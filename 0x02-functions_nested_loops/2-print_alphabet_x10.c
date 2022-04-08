8280060f3ef82e9a37937a53b8a1d6ba8b8eedc3
lizmwale-3 <elizabethmwale3@gmail.com>
2022-04-08 18:33:02 +0300

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
- *
  * print_alphabet_x10 - a function that prints 10 times the alphabet
  *
  * Return: x10 a-z
  */
 void print_alphabet_x10(void)
 {
-int n;
+int n, i; 
 
 i = 0;
 
