25a12b9999674e0d8ceb8dbfad8efed25b8bbf80
lizmwale-3 <elizabethmwale3@gmail.com>
2022-06-27 14:23:54 +0300

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -25,12 +25,10 @@ int find_len(char *str)
 }
 
 /**
- * create_xarray - Creates an array of chars and initializes it with
- *                 the character 'x'. Adds a terminating null byte.
+ * create_xarray - Creates an array of chars and initializes it with the character 'x'. Adds a terminating null byte. 
  * @size: The size of the array to be initialized.
  *
- * Description: If there is insufficient space, the
- *              function exits with a status of 98.
+ * Description: If there is insufficient space, the function exits with a status of 98.
  * Return: A pointer to the array.
  */
 char *create_xarray(int size)
